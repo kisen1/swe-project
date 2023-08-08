f70cb048afba8677e122a1bc2db73a7ebea8c263
Yonas Abraha <yonininu@gmail.com>
2023-08-08 00:39:15 -0500

    Adding repository and service interfaces

--- cs425-swe-project-WebShopApp/src/main/java/com/example/cs425sweprojectwebshopapp/repository/IEmployeeRepository.java
+++ cs425-swe-project-WebShopApp/src/main/java/com/example/cs425sweprojectwebshopapp/repository/IEmployeeRepository.java
@@ -1,10 +1,10 @@
 package com.example.cs425sweprojectwebshopapp.repository;
 
-import com.example.cs425sweprojectwebshopapp.model.Customer;
+import com.example.cs425sweprojectwebshopapp.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ICustomerRepository extends JpaRepository<Customer,Long>{
+public interface IEmployeeRepository extends JpaRepository<Employee,Long>{
 
 }
--- cs425-swe-project-WebShopApp/src/main/java/com/example/cs425sweprojectwebshopapp/service/IPaymentInfoService.java
+++ cs425-swe-project-WebShopApp/src/main/java/com/example/cs425sweprojectwebshopapp/service/IPaymentInfoService.java
@@ -3,5 +3,5 @@ package com.example.cs425sweprojectwebshopapp.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface ICustomerService {
+public interface IPaymentInfoService {
 }
